5eabecf9a7b6400ae5468b75fdd28e9b92f97243
Kamol <63007078+kmlo8@users.noreply.github.com>
2025-11-22 22:05:28 +0900

    Clean up next.config.mjs by removing comments
    Removed commented-out code for nextConfig.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,18 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'image.tmdb.org',
-//         port: '',
-//         pathname: '/t/p/w500/**',
-//       },
-//     ],
-//   },
-// };
-//
-// export default nextConfig;
 
 
 /** @type {import('next').NextConfig} */
